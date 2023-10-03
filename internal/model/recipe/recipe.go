a949b704f692ffde1496c47d8dd6732a84626f62
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-10-03 18:06:36 +0300

    added openapi specs

--- internal/model/recipe/recipe.go
+++ internal/model/recipe/recipe.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
+	ID           string    `json:"id" swag:"-"`
+	Name         string    `json:"name" `
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	PublishedAt  time.Time `json:"publishedAt" swag:"-"`
 }
--- internal/recipe/delivery/http/handlers.go
+++ internal/recipe/delivery/http/handlers.go
@@ -17,6 +17,13 @@ type apiHandler struct {
 func NewRecipeHandler() recipe.Handlers {
 	return &apiHandler{}
 }
+
+// @Summary      Create a New Recipe
+// @Description  Create a new recipe by providing Recipe JSON data and store it in the database. Returns the saved recipe as JSON.
+// @Produce      json
+// @Param        Recipe  body  model.Recipe  true  "Recipe JSON"
+// @Success      200   {object}   model.Recipe
+// @Router       /recipes [post]
 func (h *apiHandler) CreateRecipeHandler(c *gin.Context) {
 	var rec model.Recipe
 	if err := c.ShouldBindJSON(&rec); err != nil {
@@ -29,19 +36,35 @@ func (h *apiHandler) CreateRecipeHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, rec)
 }
+
 func (h *apiHandler) ListRecipeHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, []model.Recipe{})
 }
+
+// @Summary      Update Recipe
+// @Description  Update an existing recipe by providing updated Recipe JSON data. Returns updated recipe upon successful update.
+// @Produce      json
+// @Param        Recipe  body  model.Recipe  true  "Updated Recipe JSON"
+// @Success      200  {object}  model.Recipe
+// @Router       /recipes [put]
 func (h *apiHandler) UpdateRecipeHandler(c *gin.Context) {
+
 	var recipe model.Recipe
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	//sent to service
-	c.JSON(http.StatusOK, []model.Recipe{})
+	c.JSON(http.StatusOK, model.Recipe{})
 }
+
+// @Summary      Delete Recipe
+// @Description  Delete a recipe by providing its unique identifier (ID). Returns a confirmation message upon successful deletion.
+// @Produce      json
+// @Param        id  path  string  true  "Recipe ID"
+// @Success      200   {object}   map[string]interface{}
+// @Router       /recipes/{id} [delete]
 func (h *apiHandler) DeleteRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
 
@@ -49,6 +72,13 @@ func (h *apiHandler) DeleteRecipeHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Recipe with id (%s) has been deleted", id)})
 }
+
+// @Summary      Search Recipes
+// @Description  Search for recipes based on specified tags. Returns a list of matching recipes.
+// @Produce      json
+// @Param        tags  query  string  true  "Comma-separated list of tags for filtering"
+// @Success      200   {array}   model.Recipe
+// @Router       /recipes/search [get]
 func (h *apiHandler) SearchRecipeHandler(c *gin.Context) {
 	id := c.Query("tags")
 
