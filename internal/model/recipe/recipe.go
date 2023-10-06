19a46534748d3383208a6e4ae99c58710cb7e620
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-10-06 22:50:04 +0300

    refactor Update Recipe struct

--- internal/model/recipe/recipe.go
+++ internal/model/recipe/recipe.go
@@ -1,12 +1,16 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Recipe struct {
-	ID           string    `json:"id" swag:"-"`
-	Name         string    `json:"name" `
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt" swag:"-"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" `
+	Tags         []string           `json:"tags" `
+	Ingredients  []string           `json:"ingredients" `
+	Instructions []string           `json:"instructions" `
+	PublishedAt  time.Time          `json:"publishedAt" `
 }
