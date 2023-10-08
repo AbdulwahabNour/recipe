1e1417cb67133651c0e599c3934068237a911821
Abdulwahab Nour <abdelwahabnourr@gmail.com>
2023-10-08 23:02:49 +0300

    Integrate MongoDB and pass it to the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,19 @@ import (
 	"log"
 
 	"github.com/AbdulwahabNour/recipe/internal/server"
+	"github.com/AbdulwahabNour/recipe/pkg/db/mongo"
 )
 
 func main() {
-	server := server.NewServer()
-	err := server.Run()
+
+	mongoConn, err := mongo.NewMongoClient()
+	if err != nil {
+		panic(err)
+	}
+	mongoDB := mongoConn.Client.Database("recipes")
+
+	server := server.NewServer(mongoDB)
+	err = server.Run()
 	if err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
